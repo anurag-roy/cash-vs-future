57e96b6bb1230e3dcd0e8dc1ce6ea22205ede20f
Anurag Roy <anuragroy@duck.com>
2023-02-25 23:54:31 +0530

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
