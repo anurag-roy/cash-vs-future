dffb85af147874286c50423cec35131109da6b5d
Anurag Roy <anuragroy@duck.com>
2023-02-26 02:12:52 +0530

    Migrate to `next/font`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/globals.css';
-import { Inter } from '@next/font/google';
+import { Inter } from "next/font/google";
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 
